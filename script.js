535071cdb043c5b55181abc79f6ac6a1c706676a
Jayde Mitchell <jdmitchell@alumni.berklee.edu>
2021-03-18 18:55:16 -0700

    modern

--- script.js
+++ script.js
@@ -1,39 +1,38 @@
-// TODO: add code here
-window.addEventListener("load", () => {
-  const json = fetch(
+const asyncFunction = async () => {
+  const promiseResponse = await fetch(
     "https://handlers.education.launchcode.org/static/astronauts.json"
-  ).then((response) =>
-    response.json().then((responseJSONArray) => {
+  )
 
-      console.log(responseJSONArray)
-      const div = document.getElementById("container")
+  const json = await promiseResponse.json()
 
-      let webPage = '';
+  const div = document.getElementById("container")
 
-      const buildAstronautCard = (astronaut) => {
-        return `
+  let webPage = ""
+
+  const buildAstronautCard = (astronaut) => {
+    return `
             <div class="astronaut">
                 <div class="bio">
                     <h3>${astronaut.firstName} ${astronaut.lastName}</h3>
                     <ul>
                         <li>Hours in space: ${astronaut.hoursInSpace}</li>
                         <li>Active: ${astronaut.active}</li>
-                        <li>Skills: ${astronaut.skills}</li>
+                        <li>Skills: ${astronaut.skills.join(", ")}</li>
                     </ul>
                 </div>
                 <img class="avatar" src="${astronaut.picture}">
             </div>
-            `;
-      }
+            `
+  }
 
-      for (astronaut of responseJSONArray) {
-          const newCard = buildAstronautCard(astronaut)
-          console.log(newCard)
-          webPage += newCard
-        }
-        
-        console.log('webpage', webPage)
-      div.innerHTML = webPage
-    })
-  )
-})
+  json.forEach((astronaut) => {
+    const newCard = buildAstronautCard(astronaut)
+    console.log(newCard)
+    webPage += newCard
+  })
+
+  console.log("webpage", webPage)
+  div.innerHTML = webPage
+}
+
+asyncFunction()
